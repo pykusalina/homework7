ea93c1c871763828480bdd1cea1bdb751e8f1f78
Alina Pykus <alinapykus17@email.com>
2022-08-23 01:05:47 +0300

    add some changes

---
+++ main.js
@@ -0,0 +1,103 @@
+// Task 1
+// window.open("https://js.com", "js", "width = 200, heigth = 200");
+
+// setTimeout(function() {
+//     window.open("https://js.com", "js", "width = 500, heigth = 500");
+// }, 2000)
+
+// setTimeout(function() {
+//     window.moveTo(200, 200);
+// }, 2000)
+
+// setTimeout(function() {
+//     window.close;
+// }, 2000)
+
+// Task 2
+// let btn = document.querySelector("#btn");
+// let title = document.querySelector(".title")
+
+// function changeCSS() {
+//     btn.addEventListener("click", function(event) {
+// title.classList.add(".text");
+//     })
+// }
+
+// changeCSS();
+
+// Task 3
+//  let blue = document.querySelector("#btnblue");
+//  let first = document.querySelector(".first");
+
+//  let pink = document.querySelector('#btnpink');
+//  let second = document.querySelector(".second");
+
+//  let brown = document.querySelector('#btnbrown');
+//  let third = document.querySelector(".third");
+
+//  let link = document.querySelector('#link');
+//  let fourth = document.querySelector(".fourth");
+
+//  blue.addEventListener("click", function(event) {
+//     first.classList.add(".blue");
+//  })
+
+// pink.addEventListener("dblclick", function(event) {
+// second.classList.add(".pink")
+// })
+
+// brown.addEventListener("auxclick", function(event) {
+  //  third.classList.add(".brown");
+// })
+
+// link.addEventListener("mouseover", function(event) {
+ //   fourth.classList.add(".yellow");
+// })
+
+// task 4
+// let name = document.querySelector('#namelist');
+// let btn = document.querySelector('#btn');
+
+
+// btn.addEventListener("click", function(event) {
+// name.remove();
+// })
+
+// task 5
+// let btn = document.querySelector('#btn');
+
+// btn.addEventListener("click", function(event) {
+//     alert('I was pressed!')
+// })
+
+// btn.addEventListener("mouseover", function(event) {
+//     alert('Mouse on me!')
+// })
+
+// btn.addEventListener("mouseout", function(event) {
+//     alert('Mouse is not on me!')
+// })
+
+// task 6
+// alert(screen.availHeight);
+// alert(screen.availWidth);
+
+// task 7
+//  let country = document.querySelector('#country');
+//  let cities = document.querySelector("#cities");
+ 
+
+//  function change() {
+
+//  if (country.value ===  "Germany") {
+//     cities.value === "Berlin";
+//  } else if (country.value === "USA") {
+//      cities.value === "new-Yourk";
+//  } else (country.value === "Ukraine") 
+//      cities.value === "Kyiv";
+
+
+//  }
+
+//   country.addEventListener("click", change);
+
